32aa7f384737c1af3de45917419a4ebfd2ab6fa8
Sergey <serega200102@gmail.com>
2021-02-07 19:50:15 +0300

    Доделал 1 страницу сайта

--- start/js/main.js
+++ start/js/main.js
@@ -8,5 +8,19 @@ $(function(){
       }
   });
 
-  
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+
+    prevArrow:'<button  class=" slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow:'<button  class=" slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.heroes__slider-img',
+    arrows:false
+  });
 });
